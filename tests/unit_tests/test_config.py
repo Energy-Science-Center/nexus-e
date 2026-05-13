ce6e61d98a1c87d561d23600a0e706b49c4bbc6b
Matthieu Boubat <matthieu@boubat.ch>
2026-05-13 16:49:47 +0200

    Add CLI commands to change config parameters

--- tests/unit_tests/test_config.py
+++ tests/unit_tests/test_config.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import os
 from pathlib import Path
 import shutil
@@ -7,6 +8,7 @@ import src.nexus_e.config as config
 
 TEST_DATA_FOLDER = Path() / "tests" / "tmp"
 """Everything in this folder is deleted after running the tests"""
+TEST_FILE = TEST_DATA_FOLDER / "test_to_delete.toml"
 
 @pytest.fixture(scope="function", autouse=True)
 def cleanup(request: pytest.FixtureRequest):
@@ -122,3 +124,89 @@ class TestConfig:
         # Assert
         assert result.modules.commons["any_parameter"] == "any_value"
         assert result.modules.commons["another_parameter"] == "another_value"
+
+
+class TestConfigCli:
+    @pytest.mark.parametrize(("raw_args", "expected_config"),
+        [
+            (
+                [
+                    "config",
+                    "--data_context", "name", "any_name",
+                    "--data_context", "host", "any_host",
+                ],
+                {
+                    "data_context": {
+                        "name": "any_name",
+                        "host": "any_host"
+                    }
+                }
+            ),
+            (
+                [
+                    "config",
+                    "--modules", "playlist_name", "any_name",
+                    "--modules-commons", "any_parameter", "any_value",
+                ],
+                {
+                    "modules": {
+                        "playlist_name": "any_name",
+                        "commons": {
+                            "any_parameter": "any_value"
+                        }
+                    }
+                }
+            ),
+            (
+                [
+                    "config",
+                    "--modules-commons", "any_integer", "1",
+                    "--modules-commons", "any_float", "1.2",
+                    "--modules-commons", "any_boolean", "true",
+                    "--modules-commons", "another_boolean", "false",
+                ],
+                {
+                    "modules": {
+                        "commons": {
+                            "any_integer": 1,
+                            "any_float": 1.2,
+                            "any_boolean": True,
+                            "another_boolean": False,
+                        }
+                    }
+                }
+            ),
+        ]
+    )
+    def test_update_config_parameters_through_cli(
+        self,
+        raw_args: list[str],
+        expected_config: dict
+    ):
+        # Arrange
+        config_file_path = TEST_DATA_FOLDER / "test_to_delete.toml"
+        config_file = config.TomlFile(config_file_path)
+        default_config = config.Config()
+        config.write(default_config, config_file)
+        raw_args.append("--file")
+        raw_args.append(str(config_file_path.absolute()))
+        parser = ArgumentParser()
+        config.Cli.add_to_parser(parser.add_subparsers())
+        sut = parser.parse_args(raw_args)
+        expected_results = config.Config()
+        expected_results.parse(**expected_config)
+                
+        # Act
+        sut.start_execution_mode(sut.__dict__)
+        results = config.load(config_file)
+
+        # Assert
+
+        # check that expected_results.modules.commons dict is a subset of the
+        # results.modules.commons dict
+        # Necessary because the dict given as a pytest parameter overwrites
+        # the default items in Config.modules.commons dict
+        assert expected_results.modules.commons.items() <= results.modules.commons.items()
+        expected_results.modules.commons = results.modules.commons
+
+        assert results == expected_results
